48b086df2be973433d29ce70bb0c2a9fcf3e7849
Juan A. Sandino B. <juan.armando.sandino.ba@outlook.com>
2024-03-02 13:24:55 -0600

    refactor chore;add NewtonsoftJson package

--- obj/Debug/net8.0/ClientNote-API-EF.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClientNote-API-EF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientNote-API-EF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15883fc45e6aa0de88e0a9831d5336fcc7c1d303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b88364e6f91f936cf8e2f68a2d371cc4f9694d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientNote-API-EF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientNote-API-EF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
