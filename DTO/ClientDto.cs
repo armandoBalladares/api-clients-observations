5ddeb0654d1448cb50b704fcedf4d29b80595c83
Juan A. Sandino B <juan.armando.sandino.ba@outlook.com>
2024-03-02 13:56:44 -0600

    refactor chore; add DTOs

---
+++ DTO/ClientDto.cs
@@ -0,0 +1,18 @@
+﻿using ClientNote_API_EF.Models;
+using System.Security.Policy;
+
+namespace ClientNote_API_EF.DTO
+{
+    public class ClientDto {
+        public required string Name { get; set; }
+        public string? LastName { get; set; } = null!;
+        public required string Phone { get; set; }
+        public string? Email { get; set; } = null!;
+        public bool? Archived { get; set; } = false;
+        public DateTime?  Created { get; set; } = DateTime.Now;
+        public string? Updated { get; set; } = DateTime.Now.ToString();
+        public string? Deleted { get; set; } = DateTime.Now.ToString();
+        public ICollection<CommentDto>? Comments { get; set; }
+
+    }
+}
---
+++ DTO/CommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientNote_API_EF.DTO
+{
+    public class CommentDto
+    {
+        public required string Description { get; set; }
+
+    }
+}
