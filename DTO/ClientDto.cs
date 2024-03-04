5deb88cdb078ddf8aec74fbd441b6c1c4f6f839d
Juan A. Sandino B <juan.armando.sandino.ba@outlook.com>
2024-03-04 14:49:19 -0600

    refactor chore;  modify DTo for client, add field id for update data(put method) and remove value for updated and deleted fields

--- DTO/ClientDto.cs
+++ DTO/ClientDto.cs
@@ -4,14 +4,15 @@ using System.Security.Policy;
 namespace ClientNote_API_EF.DTO
 {
     public class ClientDto {
+        public int? Id { get; set; }
         public required string Name { get; set; }
         public string? LastName { get; set; } = null!;
         public required string Phone { get; set; }
         public string? Email { get; set; } = null!;
         public bool? Archived { get; set; } = false;
         public DateTime?  Created { get; set; } = DateTime.Now;
-        public string? Updated { get; set; } = DateTime.Now.ToString();
-        public string? Deleted { get; set; } = DateTime.Now.ToString();
+        public string? Updated { get; set; }
+        public string? Deleted { get; set; }
         public ICollection<CommentDto>? Comments { get; set; }
 
     }
